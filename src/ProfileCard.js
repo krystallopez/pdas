1575fd0f86711fa8cef843fc783bd5391749a037
Krystal <lopezkrystal10@gmail.com>
2023-06-21 15:05:45 -0700

    destructured props and cleaned up code

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-// creates ProfileCard info, passing props into the function 
-function ProfileCard(props) {
+// creates ProfileCard info, passing props into the function
+// destructured props passed in as argument in function 
+function ProfileCard({title, handle}) {
+
   return (
     <>
-      <div>
-        Title is : {props.title}
-      </div>
-      <div>Handle is: {props.handle}</div>
+      <div>Title is : {title}</div>
+      <div>Handle is: {handle}</div>
     </>
   );
 }
