ab1ec9c5f1ad166fbfe390913a13f9d1816f92a2
Krystal <lopezkrystal10@gmail.com>
2023-06-22 12:11:19 -0700

    import bulma css into app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
-
 
 //this function shows what the user sees on the homepage of the app
 function App() {
@@ -11,11 +11,11 @@ function App() {
     <div>
       <div>Personal Digitial Assistant</div>
       <br></br>
-      <ProfileCard title="Alexa" handle="@alexa99"  image={AlexaImage}/>
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
       <br></br>
       <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
       <br></br>
-      <ProfileCard title="Siri" handle="@siri01"  image={SiriImage}/>
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
