b2a9fbecd6128c624afb208707eaa19f90f383a5
Krystal <lopezkrystal10@gmail.com>
2023-06-22 11:49:01 -0700

    added images to src and passed image props in App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
 
 //this function shows what the user sees on the homepage of the app
 function App() {
   return (
-
-   <div>
-    <div>Personal Digitial Assistant</div>
-    <br></br>
-    <ProfileCard  title="Alexa" handle="@alexa99"/>
-    <br></br>
-    <ProfileCard title="Cortana" handle="@cortana32"/>
-    <br></br>
-    <ProfileCard  title="Siri" handle="@siri01"/>
-   </div>
-
+    <div>
+      <div>Personal Digitial Assistant</div>
+      <br></br>
+      <ProfileCard title="Alexa" handle="@alexa99"  image={AlexaImage}/>
+      <br></br>
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+      <br></br>
+      <ProfileCard title="Siri" handle="@siri01"  image={SiriImage}/>
+    </div>
   );
 }
 
