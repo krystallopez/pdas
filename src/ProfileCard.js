ed67b6a168a6d24d5867604e2186b2f0555e1eb1
Krystal <lopezkrystal10@gmail.com>
2023-06-22 12:06:21 -0700

    added image prop to profile card and installed bulma css

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 // creates ProfileCard info, passing props into the function
-// destructured props passed in as argument in function 
-function ProfileCard({title, handle}) {
-
+// destructured props passed in as argument in function
+function ProfileCard({ title, handle, image }) {
   return (
     <>
-      <div>Title is : {title}</div>
-      <div>Handle is: {handle}</div>
+      <div>
+        <img src={image} alt="pda logo"/>
+        <div>Title is : {title}</div>
+        <div>Handle is: {handle}</div>
+      </div>
     </>
   );
 }
