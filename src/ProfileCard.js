1fa0cd59e48a9c39ac4d7e64bdbb0e5b2c953716
Krystal <lopezkrystal10@gmail.com>
2023-06-22 13:37:02 -0700

    added bulma styling to profilecard and app components

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -5,10 +5,18 @@ import React from "react";
 function ProfileCard({ title, handle, image }) {
   return (
     <>
-      <div>
-        <img src={image} alt="pda logo"/>
-        <div>Title is : {title}</div>
-        <div>Handle is: {handle}</div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={image} alt="pda logo" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">Title is : {title}</p>
+            <p className="subtitle is-6">Handle is: {handle}</p>
+          </div>
+        </div>
       </div>
     </>
   );
